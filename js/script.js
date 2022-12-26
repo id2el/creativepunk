331454d09d15a947ea94787873621bb2096c5715
Id2al <118166181+id2el@users.noreply.github.com>
2022-12-26 16:25:54 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  	$("#typed").typed({
+  		strings: ["01001100011011110110000101100100011010010110111001100111", "Administrator", "Login", "Hello:)"],
+  		typeSpeed: 5,
+  		startDelay: 0,
+  		loop: true,
+  		cursorChar: "|",
+  		contentType: 'html'
+  	});
